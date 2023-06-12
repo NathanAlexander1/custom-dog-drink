d328fdd81e2d436e36fea652b36ef9843142d8bd
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-06-11 22:15:10 -0700

    Render custom drink ingredients list based on DNA test results

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useInsertionEffect, useState } from "react";
 
 function Cocktail(props) {
-    let dogBreeds = props.dogBreeds;
-    let breedMakeupArray = props.breedMakeupArray;
-    // console.log(dogBreeds)
-    const [breedsToDrinkIngredientsArray, setBreedsToDrinkIngredientsArray] = useState([]);
-    let testDataArray = ["Vodka", "Orange Juice", "Vermouth", "Tomato juice", "Bananas", "Crappy Beer", "Lil gitta ganja"]
+  let dogBreeds = props.dogBreeds;
+  let breedMakeupArray = props.breedMakeupArray;
+  // let breedPercentage =
+  console.log(props.breedMakeupArray);
+  const [breedsToDrinkIngredientsArray, setBreedsToDrinkIngredientsArray] =
+    useState([]);
+  let testDataArray = [
+    "Vodka",
+    "Orange Juice",
+    "Vermouth",
+    "Tomato juice",
+    "Bananas",
+    "Crappy Beer",
+    "Lil bitta ganja",
+  ];
+
+  const convertBreedToIngredient = dogBreeds.map((DB) => {
+    DB = testDataArray[Math.floor(Math.random() * testDataArray.length)];
+    return DB;
+  });
+
+  // console.log(convertBreedToIngredient)
+  const calculateCocktail = () => {
+    setBreedsToDrinkIngredientsArray (breedMakeupArray.map((BMA) => {
+      BMA.breedName =
+        convertBreedToIngredient[
+          Math.floor(Math.random() * convertBreedToIngredient.length)
+        ];
+      return `${BMA.breedName} - ${BMA.percentage}%`;
+    }));
+
+    console.log(breedsToDrinkIngredientsArray);
+  };
   return (
     <div>
-    {/* //take the array of all breeds
+      {/* //take the array of all breeds
     //Create new array (useState)
     //Iterate over dog breed array while simultaneously randomly iterating over testDataArray
     //For each value of dog breed array, move a random cocktail array into the new array
@@ -20,21 +48,22 @@ function Cocktail(props) {
     //Iterate over breedMakeupArray and move 
     //For each value, take a value from cocktail array and move to newly created array
         //Can code so there are never repeats
-    //Rendder Array of custom cocktailt to the screen */}
+    // //Rendder Array of custom cocktailt to the screen */}
+
+      {/* {breedMakeupArray.map((BMA, i) => {
+    console.log(BMA)
+    return
+    (<div></div>)
+})}
+
     {dogBreeds.map((DB, i) => {
-        // testDataArray.map((TDA) => {
-        //     console.log(`i - TDA`)
-        //     return (
-        //         <div><h1>{TDA}</h1></div>
-        //     )
-        // })
         DB = testDataArray[Math.floor(Math.random()*testDataArray.length)]
         return (
-            <div><h1>{DB}</h1></div>
+            // <div><h1 key={"a" + i}>{DB}</h1></div>
         )
-    })}
+    })} */}
 
-        {/* {testDataArray.map((TDA, i) => {
+      {/* {testDataArray.map((TDA, i) => {
 
             dogBreeds.map((DB) => {
                 console.log(`i - DB`)
@@ -46,10 +75,18 @@ function Cocktail(props) {
                 <div><h1>{TDA}</h1></div>
             )
     })} */}
-
-    <button >Create Your Custom Cocktail!</button>
+      {breedsToDrinkIngredientsArray.map((AITFNAR) => {
+        return (
+          <ul>
+            <li>{AITFNAR}</li>
+          </ul>
+        );
+      })}
+      <button onClick={() => calculateCocktail()}>
+        Create Your Custom Cocktail!
+      </button>
     </div>
-  )
+  );
 }
 
-export default Cocktail
+export default Cocktail;

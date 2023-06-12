62ff1aaaecc7afc41661f28f7f2de1925acb38e5
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-06-11 21:19:01 -0700

    Dogbreed array now converts to randomly assigned cocktail ingredients

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useEffect, useInsertionEffect, useState } from "react";
 
 function Cocktail(props) {
-    console.log(props)
-
+    let dogBreeds = props.dogBreeds;
+    let breedMakeupArray = props.breedMakeupArray;
+    // console.log(dogBreeds)
+    const [breedsToDrinkIngredientsArray, setBreedsToDrinkIngredientsArray] = useState([]);
     let testDataArray = ["Vodka", "Orange Juice", "Vermouth", "Tomato juice", "Bananas", "Crappy Beer", "Lil gitta ganja"]
   return (
-
-    //take the array of all breeds
+    <div>
+    {/* //take the array of all breeds
     //Create new array (useState)
     //Iterate over dog breed array while simultaneously randomly iterating over testDataArray
     //For each value of dog breed array, move a random cocktail array into the new array
@@ -17,8 +20,35 @@ function Cocktail(props) {
     //Iterate over breedMakeupArray and move 
     //For each value, take a value from cocktail array and move to newly created array
         //Can code so there are never repeats
-    //Rendder Array of custom cocktailt to the screen
+    //Rendder Array of custom cocktailt to the screen */}
+    {dogBreeds.map((DB, i) => {
+        // testDataArray.map((TDA) => {
+        //     console.log(`i - TDA`)
+        //     return (
+        //         <div><h1>{TDA}</h1></div>
+        //     )
+        // })
+        DB = testDataArray[Math.floor(Math.random()*testDataArray.length)]
+        return (
+            <div><h1>{DB}</h1></div>
+        )
+    })}
+
+        {/* {testDataArray.map((TDA, i) => {
+
+            dogBreeds.map((DB) => {
+                console.log(`i - DB`)
+                return (
+                    <div><h1>{DB}</h1></div>
+                )
+            })
+            return (
+                <div><h1>{TDA}</h1></div>
+            )
+    })} */}
+
     <button >Create Your Custom Cocktail!</button>
+    </div>
   )
 }
 
